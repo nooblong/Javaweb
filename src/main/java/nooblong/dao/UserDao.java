50a80949521579b5f97500db3df71da2095d788f
龙打野 <18128966990@163.com>
2020-06-29 05:53:59 +0800

    page-register

---
+++ src/main/java/nooblong/dao/UserDao.java
@@ -0,0 +1,30 @@
+package nooblong.dao;
+
+import nooblong.domain.User;
+import nooblong.utils.JDBCUtils;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserDao {
+    //声明JDBCTemplate对象共用
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+
+    /**
+     * 登录方法
+     * @param loginUser 只有用户名和密码
+     * @return user包含全部数据
+     */
+    public User login(User loginUser){
+        try {
+            //language=MySQL
+            String sql = "select * from user where username = ? and password = ?";
+            User user = template.queryForObject(sql, new BeanPropertyRowMapper<>(User.class),
+                    loginUser.getUsername(), loginUser.getPassword());
+            return user;
+        } catch (DataAccessException e) {
+//            e.printStackTrace();//记录日志
+            return null;
+        }
+    }
+}
