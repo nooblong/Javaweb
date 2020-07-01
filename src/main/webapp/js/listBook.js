7d73b8f1a4b1b70bc2754d5dcd5935aa024c2530
龙打野 <18128966990@163.com>
2020-07-01 20:24:44 +0800

    arrange

--- src/main/webapp/js/listBook.js
+++ src/main/webapp/js/listBook.js
@@ -16,34 +16,7 @@ function listBook(data) {
             "          <p><a class=\"btn btn-primary\" href=\"#\" role=\"button\">View details &raquo;</a></p>\n" +
             "        </div>\n");
         tmp++;
-        // }
-        // } else {
-        //     for (var i = 0; i < data.length; i += 3) {
-        //         $("[id=bookStart]").append(
-        //             "<div class=\"row\">\n" +
-        //             "        <div class=\"col-lg-4\">\n" +
-        //             "          <h2>" + data[i].name + "</h2>\n" +
-        //             "          <p class=\"text-danger\">" + data[i].introduce + "</p>\n" +
-        //             "          <p>" + "<img src='img/smoke.jpg' '>" + "</p>\n" +
-        //             "          <p><a class=\"btn btn-primary\" href=\"#\" role=\"button\">View details &raquo;</a></p>\n" +
-        //             "        </div>\n" +
-        //             "        <div class=\"col-lg-4\">\n" +
-        //             "          <h2>" + data[i + 1].name + "</h2>\n" +
-        //             "          <p class=\"text-danger\">" + data[i + 1].introduce + "</p>\n" +
-        //
-        //             "          <p>" + "<img src='img/smoke.jpg' '>" + "</p>\n" +
-        //             "          <p><a class=\"btn btn-primary\" href=\"#\" role=\"button\">View details &raquo;</a></p>\n" +
-        //             "       </div>\n" +
-        //             "        <div class=\"col-lg-4\">\n" +
-        //             "          <h2>" + data[i + 2].name + "</h2>\n" +
-        //             "          <p class=\"text-danger\">" + data[i + 2].introduce + "</p>\n" +
-        //
-        //             "          <p>" + "<img src='img/smoke.jpg' '>" + "</p>\n" +
-        //             "          <p><a class=\"btn btn-primary\" href=\"#\" role=\"button\">View details &raquo;</a></p>\n" +
-        //             "        </div>\n" +
-        //             "      </div>"
-        //         );
-        //     }
+
     }
     book_html.append(
         "<div class='row'></div>"
